fceb85db2f90b65cc0fff0b9106414e97c9302cb
mdiallo98 <mamadou1237@gmail.com>
2022-07-25 14:27:21 -0400

    implementing navigation system

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import Home from './routes/homepage/home.component';
 import { Routes, Route } from 'react-router-dom';
-
-const Shop = () => {
-  return (
-    <div>
-      <h1>Hello you can shop here </h1>
-    </div>
-  );
-};
+import Navigation from './routes/navigation/navigation.component';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
-        <Route path="shop" element={<Shop />} />
-      </Route>
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 };
