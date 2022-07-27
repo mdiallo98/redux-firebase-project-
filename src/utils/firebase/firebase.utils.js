a6f8f0f5036fbd36aa4cadad1cc729308325dce3
mdiallo98 <mamadou1237@gmail.com>
2022-07-26 23:38:40 -0400

    added firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,8 +1,19 @@
 import { initializeApp } from 'firebase/app';
-
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  //...
+  apiKey: 'AIzaSyCJ9mMvP_6DPPBA48S4qAUS7YfF_XG0x3c',
+  authDomain: 'e-commerce-8c7e9.firebaseapp.com',
+  projectId: 'e-commerce-8c7e9',
+  storageBucket: 'e-commerce-8c7e9.appspot.com',
+  messagingSenderId: '11728429268',
+  appId: '1:11728429268:web:4e40c75c8bc64ef0999b41',
+  measurementId: 'G-JDR1WF2493',
 };
 
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
